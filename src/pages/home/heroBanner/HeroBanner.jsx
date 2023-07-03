28dc1ba46919087635a2fe317082c8867608e089
asifkhan <khanasifxyz@gmail.com>
2023-07-04 02:41:07 +0530

    Search Button Fixed

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,3 +1,4 @@
+
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -8,54 +9,60 @@ import ContentWrapper from "../../../components/conentWrapper/ContentWrapper";
 import useFetch from "../../../hooks/usefetch";
 
 const HeroBanner = () => {
-    const [background, setBackground] = useState("");
-    const [query, setQuery] = useState("");
-    const navigate = useNavigate();
-    const { url } = useSelector((state) => state.home);
-    const { data, loading } = useFetch("/movie/upcoming");
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home);
+  const { data, loading } = useFetch("/movie/upcoming");
 
-    useEffect(() => {
-        const bg =
-            url.backdrop +
-            data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
-        setBackground(bg);
-    }, [data]);
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
 
-    const searchQueryHandler = (event) => {
-        if (event.key === "Enter" && query.length > 0) {
-            navigate(`/search/${query}`);
-        }
-    };
+  const searchQueryHandler = () => {
+    if (query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
 
-    return (
-        <div className="heroBanner">
-            {!loading && (
-                <div className="backdrop-img">
-                    <Img src={background} />
-                </div>
-            )}
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      searchQueryHandler();
+    }
+  };
 
-            <div className="opacity-layer"></div>
-            <ContentWrapper>
-                <div className="heroBannerContent">
-                    <span className="title">Welcome.</span>
-                    <span className="subTitle">
-                        Millions of movies, TV shows and people to discover.
-                        Explore now.
-                    </span>
-                    <div className="searchInput">
-                        <input
-                            type="text"
-                            placeholder="Search for a movie or tv show...."
-                            onChange={(e) => setQuery(e.target.value)}
-                            onKeyUp={searchQueryHandler}
-                        />
-                        <button onClick={() => searchQueryHandler}>Search</button>
-                    </div>
-                </div>
-            </ContentWrapper>
+  return (
+    <div className="heroBanner">
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
         </div>
-    );
+      )}
+
+      <div className="opacity-layer"></div>
+      <ContentWrapper>
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">
+            Millions of movies, TV shows and people to discover. Explore now.
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie or TV show..."
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={handleKeyPress}
+            />
+            <button onClick={searchQueryHandler}>Search</button>
+          </div>
+        </div>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default HeroBanner;
